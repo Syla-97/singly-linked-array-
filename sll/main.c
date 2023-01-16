597b2be1527899f67152c1def6ca8a3d14f2db9d
Syla-97 <rotus7@outlook.jp>
2023-01-16 17:19:06 +0900

    Initial Commit

---
+++ sll/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sll
+//
+//  Created by Ryousuke Takahashi on 2023/01/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
